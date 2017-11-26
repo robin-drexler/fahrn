a289ce265d1978311c2312f52563e795f6904cc5
Tobias Bales <tobias.bales@gmail.com>
2017-11-26 15:21:49 +0100

    Fix linter warnings

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './routes/routes';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
