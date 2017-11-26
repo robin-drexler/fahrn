37354e4fc44f227914ed41f10b7b86f9b9227de2
Tobias Bales <tobias.bales@gmail.com>
2017-11-26 15:05:34 +0100

    Order rides by date

--- src/api.js
+++ src/api.js
@@ -1,13 +1,17 @@
 const db = window.firebase.firestore();
 
 const COLLECTION_RIDES = 'rides';
+const FIELD_DEPARTURE_TIME = 'departure_time';
 
 export function loadAndUpdateRides(callback) {
-  db.collection(COLLECTION_RIDES).onSnapshot(snapshots => {
-    callback(
-      snapshots.docs.map(snapshot => {
-        return { ...snapshot.data(), id: snapshot.id };
-      })
-    );
-  });
+  db
+    .collection(COLLECTION_RIDES)
+    .orderBy(FIELD_DEPARTURE_TIME)
+    .onSnapshot(snapshots => {
+      callback(
+        snapshots.docs.map(snapshot => {
+          return { ...snapshot.data(), id: snapshot.id };
+        })
+      );
+    });
 }
