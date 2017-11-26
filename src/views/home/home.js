8e7270a2ae8b4a03b27d54d9336a1c7facc66ce6
Tobias Bales <tobias.bales@gmail.com>
2017-11-26 14:59:10 +0100

    Style home somewhat

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,24 +1,36 @@
 import * as React from 'react';
-import { CircularProgress, List, ListItem, ListItemText } from 'material-ui';
+import {
+  Card,
+  CircularProgress,
+  List,
+  ListItem,
+  ListItemText,
+} from 'material-ui';
 
 export default class HomeView extends React.Component {
   render() {
     if (!this.props.ridesLoaded) {
-      return <CircularProgress />;
+      return (
+        <Card>
+          <CircularProgress />
+        </Card>
+      );
     }
 
     return (
-      <List>
-        {this.props.rides.map(ride => (
-          <ListItem key={ride.id}>
-            <ListItemText
-              primary={`${ride.start} to ${ride.destination} on ${
-                ride.departure_time
-              }`}
-            />
-          </ListItem>
-        ))}
-      </List>
+      <Card>
+        <List>
+          {this.props.rides.map(ride => (
+            <ListItem button key={ride.id}>
+              <ListItemText
+                primary={`${ride.start} to ${ride.destination} on ${
+                  ride.departure_time
+                }`}
+              />
+            </ListItem>
+          ))}
+        </List>
+      </Card>
     );
   }
 }
