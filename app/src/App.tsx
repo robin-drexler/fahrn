66aab700a1a9603dbd6a6ff22d5ee6aa782a94e7
Tobias Bales <tobias.bales@gmail.com>
2017-11-26 12:51:39 +0100

    Add material-ui

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,19 +1,22 @@
 import * as React from 'react';
-import './App.css';
-
-const logo = require('./logo.svg');
+import { AppBar, Card, Toolbar, Typography } from 'material-ui';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography type="title" color="inherit">
+              Fahrn
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Card>
+          <Typography>
+            To get started, edit <code>src/App.tsx</code> and save to reload.
+          </Typography>
+        </Card>
       </div>
     );
   }
